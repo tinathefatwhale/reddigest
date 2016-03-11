6550c6440493107a3d102e0e8e1d8cc65245927c
Hunter <jmiller7@trinity.edu>
2016-03-10 22:41:12 -0600

    More progress on login stuff, polished index

--- login.php
+++ login.php
@@ -1,35 +1,41 @@
 
 <?php
+
+session_start();
 //(backend) login for reddigest
-	if($_SERVER["REQUEST_METHOD"] === "POST"){
-		$servername = '192.185.225.118';
-		// $servername = 'localhost';
-		$username = 'huntermi_rddgst';
-		// $username = 'root';
-		$password = 'testaroo123';
-		// $password = 'root';
+	if(!empty($_POST["uname"])){
+		
+		// $servername = '192.185.225.118';
+		$servername = 'localhost';
+		// $username = 'huntermi_rddgst';
+		$username = 'root';
+		// $password = 'testaroo123';
+		$password = 'root';
 		
 		$db = 'huntermi_reddigest';
 		$conn = new mysqli($servername, $username, $password, $db);
-		if(isset($_POST["uname"])){
-			$u = $_POST["uname"];
-			$p = $_POST["pword"];
+	
+		$u = $_POST["uname"];
+		$p = $_POST["pword"];
 
+		$sql = "SELECT * FROM links JOIN users ON users.ID=Links.userID WHERE users.username='{$u}' AND users.password='{$p}'";
 
-			$sql = "SELECT * FROM Links JOIN Users ON Users.ID=Links.userID WHERE Users.username='{$u}' AND Users.password='{$p}'";
-			$res = $conn->query($sql);
-			$ret = "";
-			if($res->num_rows > 0){
-				while($row = $res->fetch_assoc()){
-					$ret = $ret."<tr><td>".$row["url"]."</td><td".$row["username"]."</tr>";
-
-				}
-				echo $ret;
+		$res = $conn->query($sql);
+		
+		$ret = array();
+		if($res->num_rows > 0){
+			$row = $res->fetch_assoc();
+			// echo(json_encode(array("status" => "ACCEPT", "user" => $u, "pass" => $p, "data" => $row)));
+			while($row = $res->fetch_assoc()){
+				$ret[$row["ID"]] = $row["url"];
+				//array_push($ret, $row["url"]);
+				$_SESSION["userid"] = $row["userID"];
 			}
-			else echo "DENY";
-		}
-		else echo "DENY";
-	}
-	else echo "DENY";
 
+			echo(json_encode(array("status" => "ACCEPT", "links" => json_encode($ret), "userid" => $_SESSION["userid"])));
+		}
+		else echo(json_encode(array("status" => "DENY")));
+		
+	}
+	else echo(json_encode(array("status" => "DENY")));
 ?>
