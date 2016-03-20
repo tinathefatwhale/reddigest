504e8d4d509cbc7bfe8b5f78d15188144bcf4072
Hunter <jmiller7@trinity.edu>
2016-03-20 18:16:35 -0500

    fixed authorize.php and login javascript code

--- authorize.php
+++ authorize.php
@@ -1,22 +1,22 @@
 <?php
 	if($_SERVER["REQUEST_METHOD"] === "POST"){
 		session_start();
-		$username = htmlspecialchars($_POST["uname"]);
+		$username = htmlspecialchars($_POST["username"]);
 		$fname = htmlspecialchars($_POST["fname"]);
 		$lname = htmlspecialchars($_POST["lname"]);
 		$email = htmlspecialchars($_POST["email"]);
 		$password = htmlspecialchars($_POST["password"]);
 
 		$state = crypt($username,'a!d@l');
-		$servername = 'localhost';
+		$sn = 'localhost';
 		// $username = 'huntermi_rddgst';
-		$username = 'root';
+		$u = 'root';
 		// $password = 'testaroo123';
-		$password = 'root';
+		$p = 'root';
 		$db = 'huntermi_reddigest';
-		$conn = new mysqli($servername, $username, $password, $db);
+		$conn = new mysqli($sn, $u, $p, $db);
 
-		$sql = "INSERT INTO users (username,password,state,firstname,lastname) VALUES ('{$username}','{$password}','{$state}','{$firstname}','{$lastname}')";
+		$sql = "INSERT INTO users (username,password,state,firstname,lastname) VALUES ('{$username}','{$password}','{$state}','{$fname}','{$lname}')";
 
 		$conn->query($sql);
 		$sql = "SELECT ID FROM users WHERE username = '{username}'";
@@ -29,6 +29,8 @@
 		$_SESSION["username"] = $username;
 
 		//TOD return this url to get user to allow app
-		//'https://www.reddit.com/api/v1/authorize?client_id=VJ-N3jbAJ1fjsQ&response_type=code&'.$state.'&redirect_uri=http://www.johnhuntermiller.com/reddigest&duration=permanent&scope=history'
+		$url = 'https://www.reddit.com/api/v1/authorize?client_id=VJ-N3jbAJ1fjsQ&response_type=code&'.$state.'&redirect_uri=http://www.johnhuntermiller.com/reddigest&duration=permanent&scope=history';
+		echo json_encode(array("uid"=>$uid,"state"=>$state,"username"=>$username,"url"=>$url,));
 	}
+	else echo json_encode(array("error"=>"error",));
 ?>
