c54b643b66f9be278de6f49689304b04a669a713
Evan Cofer <ecofer@trinity.edu>
2016-03-22 23:10:33 -0500

    Completed link format
    Link style is fully implemented
    Currently have stub for all parts of link.

--- row_formatting.js
+++ row_formatting.js
@@ -1,4 +1,11 @@
 var row_color_flag = true;
+var row_count = 0;
+
+function remove_link() {
+	var thing = document.getElementById("thing");
+	thing.innerHTML = "REMOVE ME PLEASE";
+	return;
+}
 
 function add_row() {
 	/*
@@ -16,21 +23,54 @@ function add_row() {
 		row_color_flag = true;
 	}
 	
+	//
+	
 	ret+='<div class = \"row\" style=\"background-color: ';
 	ret+= row_color_style;
 	ret+=';\">';
-	ret+='<div class=\"col-sm-4\">'
-	ret+='Title link goes here';
-	ret+='</div><div class=\"col-sm-push-4\">';
-	ret+='Shortened url';
-	ret+='</div></div><div class = \"row\" style=\"background-color: ';
+	ret+='<div class= \"col-md-8\" style=\"background-color: ';
 	ret+= row_color_style;
-	ret+= ';\"><div class=\"col-sm-4\">';
-	ret+='by someone X hours ago';
+	ret+=';\">';
+	ret+='<div class = \"row\" style=\"background-color: ';
+	ret+= row_color_style;
+	ret+=';\">';
+	ret+='<div class=\"col-sm-8\" style= \"font-size: 16px; vertical-align: middle;\">';
+	ret+='<a href=\"javascript:remove_link();\">';
+	ret+='&nbsp<span class=\"glyphicon glyphicon-remove-sign\" style=\"color: white;\"></span>';
+	ret+='</a>';
+	ret+='&nbsp';
+	ret+='<a href=\"';
+	ret+='http://www.google.com';//link to article.
+	ret+='\">';
+	ret+= 'Search The Internet With This';//Title of article.
+	ret+='</a>';
+	ret+='&nbsp';
+	ret+='<a href=\"';
+	ret+='http://www.google.com';//Full site link.
+	ret+='\" style=\"font-size: 12px;\">[';
+	ret+= 'google.com'; //abreviated name.
+	ret+=']</a>';
+	ret+='</div>';
+	ret+='</div><div class = \"row\" style=\"background-color: ';
+	ret+= row_color_style;
+	ret+= ';\"><div class=\"col-sm-8 col-xs-offset-1\">';
+	ret+='by ';
+	ret+='<a href=\"';
+	ret+='https://www.reddit.com/user/Chris_Hansen_AMA';//link to article.
+	ret+='\">';
+	ret+= 'Chris_Hansen_AMA';//poster of article
+	ret+='</a>';
+	ret+='&nbsp';
+	ret+='5 hours ago';//Time information
 	ret+= ' @ ';
-	ret+= 'Some Subreddit';
+	ret+='<a href=\"';
+	ret+='https://www.reddit.com/r/programmingcirclejerk/';//link to subreddit
+	ret+='\">';
+	ret+= '/r/programmingcirclejerk';//name of subreddit
+	ret+='</a>';
 	ret+='</div></div>';
-	
+	ret+='</div>'
+	ret+='</div>';
 	tableBody.innerHTML =  tableBody.innerHTML + ret;
 	return;
 }
